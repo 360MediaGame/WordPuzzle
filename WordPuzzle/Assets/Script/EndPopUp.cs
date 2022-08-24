6ed7fb9b35eb51c2d38231936f011f116006f98f
2018180011woochan <45459935+2018180011woochan@users.noreply.github.com>
2022-08-24 17:25:56 +0900

    ENDINGSCRIPTFIX

--- WordPuzzle/Assets/Script/EndPopUp.cs
+++ WordPuzzle/Assets/Script/EndPopUp.cs
@@ -52,7 +52,8 @@ public class EndPopUp : MonoBehaviour
             _text = endScript.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
             endScript_rt = endScript.GetComponent<RectTransform>();
             endScript_rt.localPosition = new Vector3(Popup1.gameObject.GetComponent<RectTransform>().localPosition.x, Popup1.gameObject.GetComponent<RectTransform>().localPosition.y - 30, 0);
-            _text.text = "오답\n" + QuestionManager.Instance.Questions[0].gameObject.name;
+            //_text.text = "<color=#ff0000>" + "오답\n" + "</color>" + QuestionManager.Instance.Questions[0].gameObject.name;
+            _text.text = /*"오답\n" + */QuestionManager.Instance.Questions[0].gameObject.name;
         }
         //
         endImage = Instantiate(QuestionManager.Instance.Questions[1], gameObject.transform.position, Quaternion.identity, GameObject.Find("Canvas").transform);
@@ -75,7 +76,7 @@ public class EndPopUp : MonoBehaviour
             _text = endScript.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
             endScript_rt = endScript.GetComponent<RectTransform>();
             endScript_rt.localPosition = new Vector3(Popup2.gameObject.GetComponent<RectTransform>().localPosition.x, Popup2.gameObject.GetComponent<RectTransform>().localPosition.y - 30, 0);
-            _text.text = "오답\n" + QuestionManager.Instance.Questions[1].gameObject.name;
+            _text.text = /*"오답\n" + */QuestionManager.Instance.Questions[1].gameObject.name;
         }
         //
         endImage = Instantiate(QuestionManager.Instance.Questions[2], gameObject.transform.position, Quaternion.identity, GameObject.Find("Canvas").transform);
@@ -98,7 +99,7 @@ public class EndPopUp : MonoBehaviour
             _text = endScript.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
             endScript_rt = endScript.GetComponent<RectTransform>();
             endScript_rt.localPosition = new Vector3(Popup3.gameObject.GetComponent<RectTransform>().localPosition.x, Popup3.gameObject.GetComponent<RectTransform>().localPosition.y - 30, 0);
-            _text.text = "오답\n" + QuestionManager.Instance.Questions[2].gameObject.name;
+            _text.text = /*"오답\n" + */QuestionManager.Instance.Questions[2].gameObject.name;
         }
         //
         endImage = Instantiate(QuestionManager.Instance.Questions[3], gameObject.transform.position, Quaternion.identity, GameObject.Find("Canvas").transform);
@@ -121,7 +122,7 @@ public class EndPopUp : MonoBehaviour
             _text = endScript.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
             endScript_rt = endScript.GetComponent<RectTransform>();
             endScript_rt.localPosition = new Vector3(Popup4.gameObject.GetComponent<RectTransform>().localPosition.x, Popup4.gameObject.GetComponent<RectTransform>().localPosition.y - 30, 0);
-            _text.text = "오답\n" + QuestionManager.Instance.Questions[3].gameObject.name;
+            _text.text = /*"오답\n" + */QuestionManager.Instance.Questions[3].gameObject.name;
         }
         //
         endImage = Instantiate(QuestionManager.Instance.Questions[4], gameObject.transform.position, Quaternion.identity, GameObject.Find("Canvas").transform);
@@ -144,7 +145,7 @@ public class EndPopUp : MonoBehaviour
             _text = endScript.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
             endScript_rt = endScript.GetComponent<RectTransform>();
             endScript_rt.localPosition = new Vector3(Popup5.gameObject.GetComponent<RectTransform>().localPosition.x, Popup5.gameObject.GetComponent<RectTransform>().localPosition.y - 30, 0);
-            _text.text = "오답\n" + QuestionManager.Instance.Questions[4].gameObject.name;
+            _text.text = /*"오답\n" + */QuestionManager.Instance.Questions[4].gameObject.name;
         }
         //
         endImage = Instantiate(QuestionManager.Instance.Questions[5], gameObject.transform.position, Quaternion.identity, GameObject.Find("Canvas").transform);
@@ -167,7 +168,7 @@ public class EndPopUp : MonoBehaviour
             _text = endScript.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
             endScript_rt = endScript.GetComponent<RectTransform>();
             endScript_rt.localPosition = new Vector3(Popup6.gameObject.GetComponent<RectTransform>().localPosition.x, Popup6.gameObject.GetComponent<RectTransform>().localPosition.y - 30, 0);
-            _text.text = "오답\n" + QuestionManager.Instance.Questions[5].gameObject.name;
+            _text.text = /*"오답\n" + */QuestionManager.Instance.Questions[5].gameObject.name;
         }
         //
         endImage = Instantiate(QuestionManager.Instance.Questions[6], gameObject.transform.position, Quaternion.identity, GameObject.Find("Canvas").transform);
@@ -190,7 +191,7 @@ public class EndPopUp : MonoBehaviour
             _text = endScript.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
             endScript_rt = endScript.GetComponent<RectTransform>();
             endScript_rt.localPosition = new Vector3(Popup7.gameObject.GetComponent<RectTransform>().localPosition.x, Popup7.gameObject.GetComponent<RectTransform>().localPosition.y - 30, 0);
-            _text.text = "오답\n" + QuestionManager.Instance.Questions[6].gameObject.name;
+            _text.text = /*"오답\n" + */QuestionManager.Instance.Questions[6].gameObject.name;
         }
         //
         endImage = Instantiate(QuestionManager.Instance.Questions[7], gameObject.transform.position, Quaternion.identity, GameObject.Find("Canvas").transform);
@@ -213,7 +214,7 @@ public class EndPopUp : MonoBehaviour
             _text = endScript.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
             endScript_rt = endScript.GetComponent<RectTransform>();
             endScript_rt.localPosition = new Vector3(Popup8.gameObject.GetComponent<RectTransform>().localPosition.x, Popup8.gameObject.GetComponent<RectTransform>().localPosition.y - 30, 0);
-            _text.text = "오답\n" + QuestionManager.Instance.Questions[7].gameObject.name;
+            _text.text = /*"오답\n" + */QuestionManager.Instance.Questions[7].gameObject.name;
         }
         //
         endImage = Instantiate(QuestionManager.Instance.Questions[8], gameObject.transform.position, Quaternion.identity, GameObject.Find("Canvas").transform);
@@ -236,7 +237,7 @@ public class EndPopUp : MonoBehaviour
             _text = endScript.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
             endScript_rt = endScript.GetComponent<RectTransform>();
             endScript_rt.localPosition = new Vector3(Popup9.gameObject.GetComponent<RectTransform>().localPosition.x, Popup9.gameObject.GetComponent<RectTransform>().localPosition.y - 30, 0);
-            _text.text = "오답\n" + QuestionManager.Instance.Questions[8].gameObject.name;
+            _text.text = /*"오답\n" + */QuestionManager.Instance.Questions[8].gameObject.name;
         }
         //
         endImage = Instantiate(QuestionManager.Instance.Questions[9], gameObject.transform.position, Quaternion.identity, GameObject.Find("Canvas").transform);
@@ -259,7 +260,7 @@ public class EndPopUp : MonoBehaviour
             _text = endScript.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
             endScript_rt = endScript.GetComponent<RectTransform>();
             endScript_rt.localPosition = new Vector3(Popup10.gameObject.GetComponent<RectTransform>().localPosition.x, Popup10.gameObject.GetComponent<RectTransform>().localPosition.y - 30, 0);
-            _text.text = "오답\n" + QuestionManager.Instance.Questions[9].gameObject.name;
+            _text.text = /*"오답\n" + */QuestionManager.Instance.Questions[9].gameObject.name;
         }
     }
 
